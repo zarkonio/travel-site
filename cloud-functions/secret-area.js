fcbf1a27fcdff9b60b93c867523db128cc9632e6
zarkonio ubuntu desktop <zarkonio@gmail.com>
2023-05-28 18:31:59 +0200

    Updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -5,9 +5,41 @@
 //   })
 // }
 
-exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
-};
+// adresa moje cloud fje:
+// https://tourmaline-pika-8c0139.netlify.app/.netlify/functions/secret-area
+
+// exports.handler = async function (event, context) {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ message: "Hello World" }),
+//   };
+// };
+
+exports.handler = function(event, context, callback){
+  const secretContent = `
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that sky is <strong>blue</strong> and two plus two equals four.</p>
+  `
+
+  let body
+
+  if(event.body){
+    body = JSON.parse(event.body)
+  } else {
+    body = {}
+  }
+
+  if(body.password == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent
+    })
+  } else {
+    callback(null, {
+      statusCode: 401  // unautorized
+    })
+  }
+}
+
+// u praksi je dobro da github repo na koji pushujemo bude private a ne public ako radimo na bekendu tj. na cloud functionima
+// za front end manje vise
