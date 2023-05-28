9229386d1a76fea191e060f1cfacf88ba8c46039
zarkonio ubuntu desktop <zarkonio@gmail.com>
2023-05-28 17:41:26 +0200

    Modified our cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,13 @@
-exports.handler = function(event, context, callback){
-  callback(null, {
+// exports.handler = function(event, context, callback){
+//   callback(null, {
+//     statusCode: 200,
+//     body: "Welcome to the super secret area"
+//   })
+// }
+
+exports.handler = async function (event, context) {
+  return {
     statusCode: 200,
-    body: "Welcome to the super secret area"
-  })
-}
+    body: JSON.stringify({ message: "Hello World" }),
+  };
+};
